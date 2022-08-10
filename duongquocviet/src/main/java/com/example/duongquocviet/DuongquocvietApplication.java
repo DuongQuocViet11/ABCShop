52593eb847c5a899c8fd919ad6d65f693166c7e1
DELL <duongquocv@gmail.com>
2022-08-10 18:49:10 +0700

    up

---
+++ duongquocviet/src/main/java/com/example/duongquocviet/DuongquocvietApplication.java
@@ -0,0 +1,13 @@
+package com.example.duongquocviet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuongquocvietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuongquocvietApplication.class, args);
+    }
+
+}
